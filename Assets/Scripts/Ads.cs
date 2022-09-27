a175edc5451340eb9a6d8367a52bcae2cff42684
DESKTOP-BF8P05H\alfam <alfamod47@yahoo.co.jp>
2022-09-27 18:08:58 +0900

    1

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Ads : MonoBehaviour
+{
+    public TextMeshProUGUI RewardStatusText;
+    public TextMeshProUGUI InterstitialStatusText;
+
+    private void Awake()
+    {
+        //IronSourceを使う為の初期化処理（第一引数に、APP KEY:の後に書いてある数字を文字列にして入れる）
+        IronSource.Agent.init("16a036d7d", IronSourceAdUnits.REWARDED_VIDEO, IronSourceAdUnits.INTERSTITIAL, IronSourceAdUnits.OFFERWALL, IronSourceAdUnits.BANNER);
+    }
+
+    void Start()
+    {
+        IsRewardReady();
+        IsInterstitialReady();
+        LoadInterstitial();
+
+        //バナー広告の表示
+        IronSource.Agent.loadBanner(IronSourceBannerSize.BANNER, IronSourceBannerPosition.BOTTOM);
+    }
+
+    void Update()
+    {
+        IsRewardReady();
+        IsInterstitialReady();
+
+        IronSourceBannerSize.BANNER.SetAdaptive(true);
+        IronSource.Agent.displayBanner();
+    }
+
+    //インターステーショナル広告のロード
+    public void LoadInterstitial()
+    {
+        IronSource.Agent.loadInterstitial();
+    }
+
+    //インターステーショナル広告の準備ができたか？どうか
+    void IsInterstitialReady()
+    {
+        InterstitialStatusText.text = IronSource.Agent.isInterstitialReady().ToString();
+    }
+
+    //インターステーショナル広告の表示
+    public void ShowInterstitial()
+    {
+        IronSource.Agent.showInterstitial();
+    }
+
+    //リワード広告の準備ができたか？どうか
+    void IsRewardReady()
+    {
+        RewardStatusText.text = IronSource.Agent.isRewardedVideoAvailable().ToString();
+    }
+
+    //リワード広告を表示
+    public void ShowReward()
+    {
+        IronSource.Agent.showRewardedVideo();
+    }
+}
